2e1b9b221d07bd9ff175c167a88e725c7fbfbb10
jeremy <jerimonski@gmail.com>
2024-07-31 16:00:19 -0400

    stil in js algorithm, next -> js and css styles

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
-const number = getElementById('number');
-const check_btn = getElementById('check_btn');
-const clear_btn = getElementById('clear_btn');
+const number = document.getElementById('number');
+const check_btn = document.getElementById('check_btn');
+const clear_btn = document.getElementById('clear_btn');
+const number_validated_box = document.getElementById('number_validated_box');
+
+
+// to certificate if there is a input
+// return is wrong
+// if (number === '') {
+//     alert("Please provide a phone number");
+//     return; 
+// }
+
+// add label valid or not number
+check_btn.addEventListener("click", () => {
+    (number_validated_box.innerHTML += `
+        <div class="number_validated_box">${number.value}</div>
+    `);
+    number_validated_box.style.display = "block";
+});
+
+// clear labels
+clear_btn.addEventListener("click", () => {
+    (number_validated_box.innerHTML = ` `)
+    number_validated_box.style.display = "none";
+});
